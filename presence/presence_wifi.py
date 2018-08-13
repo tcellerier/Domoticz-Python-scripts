757d7fc4c135f427871ab33de039c4b47f0f0c29
Thomas Cellerier <tcellerier@users.noreply.github.com>
2018-08-13 20:31:06 +0200

    amélioration résilience script presence

--- presence/presence_wifi.py
+++ presence/presence_wifi.py
@@ -5,9 +5,10 @@
 # library scapy: pip install --user scapy
 # sudo apt-get install tcpdump
 
-from multiprocessing import Process, Value
+from multiprocessing import Process
 from scapy.all import * 
 import datetime
+import time
 
 # Credentials
 import sys
@@ -17,6 +18,10 @@ from library_credentials import *
 
 #########   Wifi   #########
 macAddressesWifi = { "XX:XX:XX:XX:XX:XX" }
+
+
+# ne marche pas avec MBP (car il se réveille tout seul de temps en temps)
+# En pratique, je constate qu'un iPhone non utilisé envoie un paquet Ethernet toutes les 15 minutes et 15s maximum même s'il y a certains rares cas à +20min (testé en 02/2018) alors qu'un Android reste inactif en permanence
 
 
 def mac_detect(pkt):
@@ -43,7 +48,6 @@ def mac_detect(pkt):
     #if not now.hour >= 8:
     #    return
 
-
     delay_lastpush = (now - lasttime).total_seconds() # nombre de secondes.microsecondes
 
     if delay_lastpush >= min_delay:
@@ -51,9 +55,9 @@ def mac_detect(pkt):
         
         ###  Actions à déclencher  ###
         #print(str(now) + " " + mac + " adresse mac source détectée")
-        os.system('echo ' + str(now) + ' mac ' + mac + ' detectee >> /home/pi/domoticz/scripts/python/presence/testiphone.log')
-  
+        os.system('echo ' + str(now) + ' mac ' + mac + ' detectee >> /home/pi/domoticz/scripts/python/presence/testiphone.log') 
 
+        
 
 def initSniff():
     global lasttime, sniff_filters
@@ -62,7 +66,6 @@ def initSniff():
 
 
 def startSniff(iface = "eth0"):
-    #lasttime = vlasttime.value
     print("Sniffing started on %s ..." % iface)
     print(sniff(iface=iface, prn=mac_detect, filter=sniff_filters, store=0))
 
@@ -72,7 +75,41 @@ if __name__ == '__main__':
 
     initSniff()
 
-    # A améliorer : partager 'lasttime' entre les process
-    Process(target=startSniff,  args=('eth0', )).start()
-    Process(target=startSniff,  args=('mon0', )).start()
+    # déclaration des processus
+    process_eth0 = Process(target=startSniff,  args=('eth0', ))
+    process_mon0 = Process(target=startSniff,  args=('mon0', )) # ne marche pas avec iPhone
+
+    # démarrage processus
+    process_eth0.start()
+    process_mon0.start()
+    #print(process_eth0.pid)
+    #print(process_mon0.pid)
+
+    # Redémarrage des processus si besoin (jusqu'à N fois)
+    i = 0
+    while i < 100:
+
+        if process_eth0.is_alive() == False:
+            i = i + 1
+            print("Restart sniffing on eth0")
+            process_eth0.terminate()
+            process_eth0.join()
+            process_eth0 = Process(target=startSniff,  args=('eth0', ))
+            process_eth0.start()
+        
+        if process_mon0.is_alive() == False:
+            i = i + 1
+            print("Restart sniffing on mon0")
+            process_mon0.terminate()
+            process_mon0.join()
+            process_mon0 = Process(target=startSniff,  args=('mon0', ))
+            process_mon0.start()
+        
+        time.sleep(60)
+
+    # On termine tous les processus enfants après N tentatives
+    process_mon0.terminate()
+    process_mon0.join()
+    process_eth0.terminate()
+    process_eth0.join()
 
